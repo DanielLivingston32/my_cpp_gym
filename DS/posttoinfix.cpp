6daa1ede85e787720b69b4bc29f37cb1c4999296
Saran Sudheer <56349474+saransudheer@users.noreply.github.com>
2020-08-22 21:04:00 +0530

    Update posttoinfix.cpp

--- DS/posttoinfix.cpp
+++ DS/posttoinfix.cpp
@@ -10,36 +10,26 @@ string infixConversion(string postfix) {
 
 	stack<string> infix;
 	int n=postfix.length();
-	stringstream a(postfix);
-	int num[n];
-	char c;
-	int temp;
-	while(a >> temp){
-		num.push_back(temp);
-		a >> c;
+	int result[n];
+	for(int i=0; i<n; i++){
+	    int result[i]=stoi(postfix[i]);
 	}
-	ostringstream str1;
-  //HOW TO CONVERT A VECTOR TO A INT ARRAY
-  //CAN YOU FIX THIS CODE
-	str1 << num; 
-
-	// the str() coverts number into string 
-	string result = str1.str(); 
-   
-   for (int i=0; result[i]!='\0'; i++) {
-      if (isOperand(result[i])) {
-         string op(1, result[i]);
-         infix.push(op);
-      } else {
-         string op1 = infix.top();
-         infix.pop();
-         string op2 = infix.top();
-         infix.pop();
-         infix.push("{"+op2+postfix[i]+op1 +"}");
-      }
-   }
-   return infix.top();
+	int result[]=stoi(postfix);
+	for (int i=0; result[i]!='\0'; i++) {
+		if (isOperand(result[i])) {
+		 string op(1, result[i]);
+		 infix.push(op);
+		} else {
+		 string op1 = infix.top();
+		 infix.pop();
+		 string op2 = infix.top();
+		 infix.pop();
+		 infix.push("{"+op2+postfix[i]+op1 +"}");
+		}
+	}
+	return infix.top();
 }
+
 int main() {
    string postfix = "1+((2+3)/(2*4))";
    cout<<"The infix conversion of the postfix expression '"<<postfix<<"' is : ";
